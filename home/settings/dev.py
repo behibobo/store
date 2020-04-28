4d6069eede9fc7aac787fb29bf1fc291d926f3a4
behzad <behzadfareed@gmail.com>
2020-04-28 17:44:46 +0430

    brand image

--- home/settings/dev.py
+++ home/settings/dev.py
@@ -11,7 +11,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'store',
-        'USER': 'behzad',
+        'USER': 'bobo',
         'PASSWORD': 'p@ssw0rd',
         'HOST': '127.0.0.1',
         'PORT': '',
