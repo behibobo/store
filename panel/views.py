d72c8eec1e245d2f7cf88c2b77042c02ee1bf272
unknown <behzadfareed@gmail.com>
2020-06-07 04:08:32 -0700

    specs

--- panel/views.py
+++ panel/views.py
@@ -647,29 +647,29 @@ class SpecList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class SpecDetail(APIView):
-
-    def get_object(self, pk, id):
+    def get_object(self, pk):
         try:
-            return Variation.objects.get(pk=id)
-        except Variation.DoesNotExist:
+            return Spec.objects.get(pk=pk)
+        except Spec.DoesNotExist:
             raise Http404
 
-    def get(self, request, pk, id,format=None):
-        spec = self.get_object(pk, id)
+    def get(self, request, pk, format=None):
+        print("here")
+        spec = self.get_object(pk)
         serializer = SpecSerializer(spec)
         return Response(serializer.data)
 
 
-    def put(self, request, pk, id, format=None):
-        spec = self.get_object(pk, id)
+    def put(self, request, pk,  format=None):
+        spec = self.get_object(pk)
         serializer = SpecSerializer(spec, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk, id, format=None):
-        variation = self.get_object(pk, id)
+    def delete(self, request, pk, format=None):
+        variation = self.get_object(pk)
         variation.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
